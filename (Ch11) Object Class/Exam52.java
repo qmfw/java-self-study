9dc2b9f0248697d8ae6f6170dc8defcd47dc6423
qmfw <qmfw79@gmail.com>
2021-09-22 01:40:05 +0900

    object

---
+++ (Ch11) Object Class/Exam52.java
@@ -0,0 +1,37 @@
+class Calculator{
+    int left, right;
+      
+    public void setOprands(int left, int right){
+        this.left = left;
+        this.right = right;
+    }
+    public void sum(){
+        System.out.println(this.left+this.right);
+    }
+      
+    public void avg(){
+        System.out.println((this.left+this.right)/2);
+    }
+     
+    public String toString(){
+        return "left : " + this.left + ", right : "+ this.right;
+    }
+}
+  
+public class Exam52 {
+      
+    public static void main(String[] args) {
+          
+        Calculator c1 = new Calculator();
+        c1.setOprands(10, 20);
+        System.out.println(c1);
+        System.out.println(c1.toString());
+    }
+  
+}
+
+/* toString 자체가 우선순위가 높기 때문에
+left : 10, right : 20
+left : 10, right : 20
+*/
+
---
+++ (Ch11) Object Class/Exam53.java
@@ -0,0 +1,25 @@
+class Student{
+    String name;
+    Student(String name){
+        this.name = name;
+    }
+    public boolean equals(Object obj) {
+        Student _obj = (Student)obj;
+        return name == _obj.name;
+    }
+}
+ 
+class Exam53 {
+ 
+    public static void main(String[] args) {
+        Student s1 = new Student("egoing");
+        Student s2 = new Student("egoing");
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+ 
+    }
+ 
+}
+
+//false
+//true
